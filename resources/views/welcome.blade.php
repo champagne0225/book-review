18c66466b9aefc69d10e7bc05423add138d9d70b
tomoya.seki <te4090st@gmail.com>
2020-12-21 00:42:49 +0000

    book model

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,10 +7,25 @@
         <div class="center jumbotron">
             <div class="text-center">
                 <h1>Welcome to the Book Review</h1>
-                {{-- ユーザ登録ページへのリンク --}}
-                {!! link_to_route('signup.get', '新規会員登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
-                {{-- ログインページへのリンク --}}
-                {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-lg btn-primary']) !!}
+            </div>
+            <div class="row">
+                <div class="col-sm-6 offset-sm-3">
+                    {!! Form::open(['route' => 'login.post']) !!}
+                        <div class="form-group">
+                            {!! Form::label('email', 'メールアドレス') !!}
+                            {!! Form::email('email', old('email'), ['class' => 'form-control']) !!}
+                        </div>
+                        <div class="form-group">
+                            {!! Form::label('password', 'パスワード') !!}
+                            {!! Form::password('password', ['class' => 'form-control']) !!}
+                        </div>
+
+                        {!! Form::submit('ログイン', ['class' => 'btn btn-primary btn-block']) !!}
+                    {!! Form::close() !!}
+
+                    {{-- ユーザ登録ページへのリンク --}}
+                    <p class="mt-2">{!! link_to_route('signup.get', '新規会員登録はコチラ') !!}</p>
+                </div>
             </div>
         </div>
     @endif
